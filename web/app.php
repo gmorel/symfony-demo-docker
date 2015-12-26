75b4d1da90e773bf2844dcfa7c5112c9b7f95c3b
Guillaume MOREL <guillaume.morel@verylastroom.fr>
2015-12-27 00:12:38 +0100

    Allow to use app_dev feature with Docker

--- web/app.php
+++ web/app.php
@@ -6,7 +6,9 @@
 //   * http://symfony.com/doc/current/cookbook/configuration/front_controllers_and_kernel.html
 //   * http://symfony.com/doc/current/cookbook/configuration/environments.html
 
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 /**
  * @var Composer\Autoload\ClassLoader
@@ -14,20 +16,22 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
 
-// If your web server provides APC support for PHP applications, uncomment these
-// lines to use APC for class autoloading. This can improve application performance
-// very significantly. See http://symfony.com/doc/current/components/class_loader/cache_class_loader.html#apcclassloader
-//
-// NOTE: The first argument of ApcClassLoader() is the prefix used to prevent
-// cache key conflicts. In a real Symfony application, make sure to change
-// it to a value that it's unique in the web server. A common practice is to use
-// the domain name associated to the Symfony application (e.g. 'example_com').
-//
-// $apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
-// $loader->unregister();
-// $apcLoader->register(true);
+$host = 'example_com';
 
-$kernel = new AppKernel('prod', false);
+$env = getenv('SYMFONY_ENV') ?: 'prod';
+$debug = 'dev' === $env;
+
+if ($debug) {
+    Debug::enable();
+}
+
+if ('prod' === $env) {
+    $apcLoader = new ApcClassLoader($host . '.' . sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+}
+
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 
 // When using the HTTP Cache to improve application performance, the application
